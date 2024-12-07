963e768e1552b152543561df36800749402f5dbc
jakep <jacob@pieniazek.me>
2024-12-07 00:33:49 -0500

    Added community plugs

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,14 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.quarto" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
+  { import = "astrocommunity.completion.tabnine-nvim"},
   -- import/override with your plugins folder
 }
