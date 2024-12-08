8e7c824a462c4b2a711ad4e7e806716323e67e09
Jacob Pieniazek <jacob@pieniazek.me>
2024-12-08 00:33:07 -0500

    Modify quarto plugin

--- lua/community.lua
+++ lua/community.lua
@@ -3,52 +3,52 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.pack.quarto" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.completion.tabnine-nvim"},
+  { import = "astrocommunity.completion.tabnine-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
   {
-  "iamcco/markdown-preview.nvim",
-  build = function(plugin)
-    local package_manager = vim.fn.executable "yarn" and "yarn" or vim.fn.executable "npx" and "npx -y yarn" or false
-
-    --- HACK: Use `yarn` or `npx` when possible, otherwise throw an error
-    ---@see https://github.com/iamcco/markdown-preview.nvim/issues/690
-    ---@see https://github.com/iamcco/markdown-preview.nvim/issues/695
-    if not package_manager then error "Missing `yarn` or `npx` in the PATH" end
-
-    local cmd = string.format(
-      "!cd %s && cd app && COREPACK_ENABLE_AUTO_PIN=0 %s install --frozen-lockfile",
-      plugin.dir,
-      package_manager
-    )
-
-    vim.cmd(cmd)
-  end,
-  ft = { "markdown", "markdown.mdx" },
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  init = function()
-    local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
-    vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
-    vim.g.mkdp_port = 4949
-  end,
-  dependencies = {
-    { "AstroNvim/astroui", opts = { icons = { Markdown = "" } } },
     {
-      "AstroNvim/astrocore",
+      "quarto-dev/quarto-nvim",
+      ft = { "quarto", "qmd", "markdown" },
+      opts = {},
+      dependencies = {
+        { "hrsh7th/nvim-cmp" },
+        { "jmbuhr/otter.nvim" },
+      },
+      config = function()
+        vim.api.nvim_create_user_command(
+          "QuartoPreviewRemote",
+          function() vim.cmd ":QuartoPreview --port 4848" end,
+          { desc = "Start Quarto Preview on port 4848 for remote access." }
+        )
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
       optional = true,
       opts = function(_, opts)
-        local maps = opts.mappings
-        local prefix = "<Leader>M"
-
-        maps.n[prefix] = { desc = require("astroui").get_icon("Markdown", 1, true) .. "Markdown" }
-        maps.n[prefix .. "p"] = { "<cmd>MarkdownPreview<cr>", desc = "Preview" }
-        maps.n[prefix .. "s"] = { "<cmd>MarkdownPreviewStop<cr>", desc = "Stop preview" }
-        maps.n[prefix .. "t"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview" }
+        if opts.ensure_installed ~= "all" then
+          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+            "r",
+            "python",
+            "markdown",
+            "markdown_inline",
+            "julia",
+            "bash",
+            "yaml",
+            "lua",
+            "vim",
+            "query",
+            "vimdoc",
+            "latex",
+            "html",
+            "css",
+          })
+        end
       end,
     },
   },
-},
 }
